8b857fae9f874cb8d7f727031756482c2471ea8b
grasion <132340012+grasion@users.noreply.github.com>
2023-05-02 17:16:58 +0900

    Update 출퇴근 체크.py

--- 출퇴근 체크.py
+++ 출퇴근 체크.py
@@ -22,8 +22,15 @@ import cv2
 import subprocess
 import schedule
 import math
+import chromedriver_autoinstaller
 
-
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+driver_path = f'./{chrome_ver}/chromedriver.exe'
+if os.path.exists(driver_path):
+    print(f"chrom driver is insatlled: {driver_path}")
+else:
+    print(f"install the chrome driver(ver: {chrome_ver})")
+    chromedriver_autoinstaller.install(True)
 #chromedriver_autoinstaller.install()
 data=MIMEMultipart()
 
